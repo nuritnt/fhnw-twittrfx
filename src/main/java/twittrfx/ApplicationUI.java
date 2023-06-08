ea410d8f636ec2f360051da9839fcc7e78fb3d3e
Tuğçe Nur Taş <tugcenur@required.ch>
2023-06-08 15:49:08 +0200

    Add toolbar

--- src/main/java/twittrfx/ApplicationUI.java
+++ src/main/java/twittrfx/ApplicationUI.java
@@ -3,15 +3,18 @@ package twittrfx;
 import javafx.scene.control.Button;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
+import twittrfx.views.Toolbox;
 import twittrfx.views.ViewMixin;
 
 // change layout here
 public class ApplicationUI extends VBox implements ViewMixin {
 
     private final PresentationModel model;
+    private final Toolbox toolbox;
     // private Button button;
     // create toolbox
     // initialize toolbox here
+
 
     public ApplicationUI(PresentationModel model) {
         this.model = model;
@@ -34,5 +37,6 @@ public class ApplicationUI extends VBox implements ViewMixin {
     public void layoutControls() {
         // getChildren().addAll(button);
         // add toolbox to layout here
+        getChildren().add(toolbox);
     }
 }
---
+++ src/main/java/twittrfx/views/Toolbar.java
@@ -0,0 +1,37 @@
+package twittrfx.views;
+
+import javafx.scene.control.Button;
+import javafx.scene.layout.HBox;
+import twittrfx.PresentationModel;
+
+public class Toolbar extends HBox implements ViewMixin {
+    private Button saveButton;
+    private Button addButton;
+    private Button deteletButton;
+    private Button undoBUtton;
+    private Button redoButton;
+    private Button englishButton;
+    private Button germanButton;
+
+    private PresentationModel model;
+
+
+    public Toolbar(PresentationModel model) {
+        this.model = model;
+        init();
+    }
+
+
+    @Override
+    public void initializeControls() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'initializeControls'");
+    }
+
+    @Override
+    public void layoutControls() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'layoutControls'");
+    }
+    
+}
